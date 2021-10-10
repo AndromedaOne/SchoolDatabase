ab9269e42fa52cdf4957c5c5a222a3ec6beb12d5
William J. Schilp, PhD <william.schilp@ansys.com>
2021-10-10 19:55:59 -0400

    we had to drop the leading zero when taking the zipcode as an integer because when java sees a leading zero in an integer it thinks the value is in octal (base 8). so take the zipcode as a string and use parseInt to convert it to an integer.

--- src/SchoolDatabase.java
+++ src/SchoolDatabase.java
@@ -10,20 +10,20 @@ public class SchoolDatabase {
         m_people.add(new Student(new Name("Joe", "Sameer", "Shmoo", 
             NameSuffix.JR), 
             new Date(new GregorianCalendar(2007, 4, 15).getTimeInMillis()),
-            new PostalAddress("123 Sesame Str", "Shirley", "MA", 1464),
+            new PostalAddress("123 Sesame Str", "Shirley", "MA", "01464"),
             Grade.Freshman));
         m_people.add(new Student(new Name("Jane", "Doe"), 
         new Date(new GregorianCalendar(2005, 4, 15).getTimeInMillis()),
         new PostalAddress("Cupboard Under the Stairs", "4 Privet Drive", 
-            "Ayer", "MA", 14432),
+            "Ayer", "MA", "01432"),
             Grade.Junior));
         m_people.add(new Student(new Name("Alicia", "Augello", "Keys"), 
             new Date(new GregorianCalendar(2004, 6, 12).getTimeInMillis()),
             new PostalAddress("1313 Mockingbird Lane", "Shirley", "MA", 
-                1464), Grade.Senior));
+                "01464"), Grade.Senior));
         m_people.add(new Student(new Name("Bartholomew", "Jojo", "Simpson"), 
             new Date(new GregorianCalendar(2005, 8, 2).getTimeInMillis()), 
-            new PostalAddress("742 Evergreen Terrace", "Ayer", "MA", 1432), 
+            new PostalAddress("742 Evergreen Terrace", "Ayer", "MA", "01432"), 
             Grade.Junior));
         for(Person person : m_people) 
         {
